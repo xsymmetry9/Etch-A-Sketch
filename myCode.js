eb60350136278e6c153f3d9e3828cc782f617cdd
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2022-05-08 22:53:45 +0800

    Add files via upload
    Added flex boxes and replace Grid
    Added Random Color
    Added "Darker" and "Lighter" for color opacity
    Added bootstraps

--- myCode.js
+++ myCode.js
@@ -1,102 +1,176 @@
-const container = document.getElementById('game-grid');
-const btnReset = document.querySelector('#reset');
-var color = RandomColor();
-var gridValue = 16;
+const buttons = document.querySelectorAll('button')
+const gameScreen = document.querySelector('.game-container')
+const getGrid = document.querySelector('.customGrid');
+// -----------Constant Variables --------------------------------------
 
-//Create grid
-function createGrid(gridValue){
-    container.style.gridTemplateColumns = `repeat(${gridValue},1fr)`;
-    container.style.gridTemplateRows = `repeat(${gridValue},1fr)`;
+var color = "default";
+// 16x16 grid
+const gridLayout = 16;
+const colRow = gridLayout;
 
-    let gridNumber = gridValue * gridValue;
-    for (let i = 1; i <=gridNumber; i++){
-        let getSquare = document.createElement('grid');
-        getSquare.classList.add('grid');
-        container.appendChild(getSquare);
+//---------------------------Create Grid-------------------------------
+
+//Draws a standard 16 x 16 Grid
+function drawSquare(num) {
+  const sketchGrid = document.querySelectorAll('.sketchGrid');
+
+  var colRow = Math.floor(400 / num);
+
+
+  const grid = document.createElement('div');
+  grid.classList.add('sketchGrid');
+  grid.setAttribute('id', 'myId');
+  grid.setAttribute('style', `height: ${colRow}px; width: ${colRow}px; outline: thin solid; outline-color: #0E185F`);
+  gameScreen.appendChild(grid);
+}
+
+function drawGrid(num) {
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      drawSquare(num);
     }
-
-//Colors the pixel
-let pixel = container.querySelectorAll(".grid");
-pixel.forEach(pixel => pixel.addEventListener('mouseover',function (e){
-    e.target.style.background = color;
-}));
+  }
 }
-createGrid(10);
+drawGrid(colRow);
 
-// ChangeBackground color to white
-function clearGrid(){
-    let grid = container.querySelectorAll(".grid");
-    grid.forEach(grid => grid.style.backgroundColor = 'white');
+// ---------------slider-------------------------------------
+const slider = document.querySelector('.slider');
+const gridInfo = document.querySelector('.info');
+slider.addEventListener('click', function(e)
+{
+  clearGrid();
+  var getValue = e.target.value; //gets value of grid
+  drawGrid((e.target.value));
+  document.querySelector('.info').innerHTML = `${getValue} X ${getValue}`;
+  const selectedGrid = document.querySelectorAll('.sketchGrid');
+  selectedGrid.forEach((element) => {
+    element.addEventListener('mouseover', changeColor);
+  });
+
+})
+//--------Change Colors----------------------//
+//Calls all buttons
+buttons.forEach((button) => {
+  button.addEventListener('mousedown', () => {
+    if (button.id == "black") {
+      color = "black";
+      console.log(color);
+    } else if (button.id == "erase") {
+      color = "erase";
+      console.log(color);
+    } else if (button.id == "random") {
+      color = "random";
+      console.log(color);
+    } else if (button.id == "lighter") {
+      color = "lighter";
+    } else if (button.id == "darker"){
+      color = "darker";
+    }
+    // Deletes the attribute of Grid
+    else if (button.id == "clear") {
+      clearGrid();
+      drawGrid(colRow);
+      const selectedGrid = document.querySelectorAll('.sketchGrid');
+      selectedGrid.forEach((element) => {
+        element.addEventListener('mouseover', changeColor);
+      });
+    }
+  });
+});
+
+//Choose color randomly
+function randomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+
+  return (`rgba(${r},${g},${b})`);
 }
 
-//Get random Colors
-function RandomColor(){
-    const r = Math.floor(Math.random()*256);
-    const g = Math.floor(Math.random()*256);
-    const b = Math.floor(Math.random()*256);
+// ---------------------- RGBA Functions -----------------------
+// Finds the value of R, G, B, A
+function getRGB(string) {
+  var start = (string.indexOf('(') + 1);
+  var end = (string.indexOf(')'));
+  var getRgba = (string.slice(start, end)).split(',');
 
-    return ("rgb(" + r + "," + g + "," + b + ")");
+  return (getRgba);
 
 }
-const btns = document.querySelectorAll('button');
-
-function changeColor(e){
-
-    if (e.target.id =="black")
+// Change the opacity to 10% lighter
+function lighter(myArray){
+    var r = myArray[0];
+    var g = myArray[1];
+    var b = myArray[2];
+    if (myArray.length == 3)
     {
-        color = ("black");
+        myArray.push(.9);
+        var a  = myArray[3];
+        return `rgba(${r}, ${g}, ${b},${a})`;
     }
-
-    else if(e.target.id =="RandomColor")
-    {
-        color = RandomColor();
-    }
-
-    else if(e.target.id == "reset")
-    {
-        return clearGrid();
+    else{
+      if(myArray[3] > .1){
+        myArray[3] = myArray[3] - .1;
+        return `rgba(${r}, ${g}, ${b},${myArray[3]})`;
+      }
     }
 }
-function getColor(){
-    return changeColor(e);
+
+// Change the opacity to 10% Darker
+
+function darker(myArray){
+    var r = myArray[0];
+    var g = myArray[1];
+    var b = myArray[2];
+    var a = myArray[3];
+    if(a <= .9 || a >= .1){
+        a = (a * 10 + 1) / 10;
+        return `rgba(${r}, ${g}, ${b},${a})`;
+      }
+    }
+
+//------Create a ChangeColor Function----------
+function changeColor(e) {
+  var getColor = this.style.backgroundColor;
+  //Checks if there is a backgroundColor attribute
+  if (getColor == '') {
+    if (color == "black") {
+      this.style.backgroundColor = `rgba(0,0,0)`;
+    }
+    if (color == "random") {
+      let getRandom = randomColor();
+
+      this.style.backgroundColor = getRandom;
+      console.log(this.style.backgroundColor)
+    }
+  } else {
+    // Erases background attributes
+    if (color == "erase") {
+      this.style.backgroundColor = '';
+    }
+    // Makes color lighter but not white
+    if (color == "lighter") {
+      var myArray = getRGB(getColor);
+      var getLighter = lighter(myArray);
+      this.style.backgroundColor = getLighter;
+      console.log(getLighter);
+    }
+    // Makes color darker
+    if (color == "darker") {
+      var myArray = getRGB(getColor);
+      var getDarker = darker(myArray);
+      this.style.backgroundColor = getDarker;
+      console.log(getDarker);
+    }
+  }
+}
+// Clear grid
+function clearGrid() {
+  gameScreen.innerHTML = '';
 }
 
-
-//Buttons functionality
-const blackbtn = document.getElementById('black');
-blackbtn.addEventListener('click',changeColor);
-
-const randomColorbtn = document.getElementById('RandomColor');
-randomColorbtn.addEventListener('click', changeColor);
-
-const resetbtn = document.getElementById('reset');
-resetbtn.addEventListener('click',changeColor)
-
-/*btns.forEach((button) => {
-    button.addEventListener('click',()=>{
-        //alert(button.id);
-        if (button.id === "black")
-        {
-            color = "black";
-        }
-        else if (button.id ==="RandomColor")
-        {
-            color = RandomColor();
-        }
-        else if (button.id === "reset")
-        {
-            clearGrid();
-        }
-    });
-});*/
-
-//btnReset.addEventListener('click', clearGrid);
-
-//Create a slider that returns the total columns x gridx
-const slider = document.getElementById('sizeSlider');
-const sizeValue = document.getElementById('sizeValue');
-slider.oninput = function(){
-    gridValue = (this.value);
-    sizeValue.innerHTML = `${this.value} x ${this.value}`;
-    createGrid(gridValue);
-}
+//Change background of grid when it's clicked
+const selectedGrid = document.querySelectorAll('.sketchGrid');
+selectedGrid.forEach((element) => {
+  element.addEventListener('mouseover', changeColor);
+});
